ddce34c1f46078bf99592a84b74b0115f35e2793
NicolasLegendre81 <86867910+NicolasLegendre81@users.noreply.github.com>
2022-01-29 09:24:30 +0100

    connection à la db avec mysql2

---
+++ backend/config/db.js
@@ -0,0 +1,19 @@
+const mysql = require ('mysql2');
+require('dotenv').config();
+//connect to the database
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER, 
+    database: 'groupomaniacs',
+    password: process.env.DB_PASSWORD
+  });
+  db.execute(function(err) {
+    if (err) {
+      console.error('La connection à la base de donnée a echoué');
+      return;
+    }
+   
+    console.log('La connection a été établie avec succés ');
+  });
+  
+module.exports = db;
