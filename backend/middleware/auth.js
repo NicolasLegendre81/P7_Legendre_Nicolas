e34fdd0452ddc5131ce4cd82c9075531c71b893a
NicolasLegendre81 <86867910+NicolasLegendre81@users.noreply.github.com>
2022-02-01 16:36:54 +0100

    Add files via upload

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -5,7 +5,6 @@ module.exports = (req, res, next) => {
     const token = req.headers.authorization.split(' ')[1];
     const decodedToken = jwt.verify(token, process.env.MY_TOKEN);
     const userId = decodedToken.userId;
-    req.userId={ userId}
     if (req.body.userId && req.body.userId !== userId) {
       throw 'userId invalide!';
     } else {
---
+++ backend/middleware/multer.js
@@ -0,0 +1,21 @@
+//import du package de gestion des fichiers
+const multer = require('multer');
+
+const MIME_TYPES = {
+    'image/jpg':'jpg',
+    'image/jpeg': 'jpg',
+    'image/png':'png'
+};
+// Configuration du chemin et du nom des fichiers entrants
+const storage = multer.diskStorage({
+    destination: (req,file,callback) =>{
+        callback(null,'images');
+    },
+    filename:(req,file,callback) => {
+        const name = file.originalname.split('').join('_');
+        const extension = MIME_TYPES[file.mimetype];
+        callback(null,name + Date.now() +'.' + extension);
+    }
+});
+
+module.exports = multer({storage:storage}).single('image');
