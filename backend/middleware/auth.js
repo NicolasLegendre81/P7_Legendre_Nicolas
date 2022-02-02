19451329558ecbb8df161bf06989fcb5b6d7b797
NicolasLegendre81 <86867910+NicolasLegendre81@users.noreply.github.com>
2022-02-02 15:56:30 +0100

    Add files via upload

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,8 +4,8 @@ module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
     const decodedToken = jwt.verify(token, process.env.MY_TOKEN);
-    const userId = decodedToken.userId;
-    if (req.body.userId && req.body.userId !== userId) {
+    const userId = decodedToken.user_id;
+    if (req.body.user_id && req.body.user_id !== userId) {
       throw 'userId invalide!';
     } else {
       next();
