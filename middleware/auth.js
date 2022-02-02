fb6c05659b1d60fa9e58bee31af530343c38330e
NicolasLegendre81 <86867910+NicolasLegendre81@users.noreply.github.com>
2022-02-02 07:36:23 +0100

    Add files via upload

---
+++ middleware/auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.verify(token, process.env.MY_TOKEN);
+    const userId = decodedToken.userId;
+    if (req.body.userId && req.body.userId !== userId) {
+      throw 'userId invalide!';
+    } else {
+      next();
+    }
+  } catch {
+    res.status(401).json({
+      error: new Error('La requête est invalide!')
+    });
+  }
+};
