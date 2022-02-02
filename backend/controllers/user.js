884f59d3105bf129671ebe4cdba523f3dcc553f8
NicolasLegendre81 <86867910+NicolasLegendre81@users.noreply.github.com>
2022-02-02 15:55:57 +0100

    Add files via upload

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -48,11 +48,12 @@ exports.login = (req, res, next) => {
                             message: 'Mot de passe incorrect.'
                         });
                     }else{
+                        console.log(results)
                         res.status(200).json({
-                            userId: results[0].id,
+                            user_id: results[0].user_id,
                             
                             token: jwt.sign({
-                                userId: results[0].id
+                                user_id: results[0].user_id
                             }, process.env.MY_TOKEN,
                              {expiresIn: '24h'})
 
@@ -65,3 +66,59 @@ exports.login = (req, res, next) => {
         }
     });
 };
+
+exports.getUserProfile = (req,res,next) => {
+    db.query(`SELECT * FROM users WHERE user_id=${req.params.id};`,(err,result) =>{
+        if(err){
+            return res.status(400).json({err});
+        }if(result){
+            return res.status(200).json(result)
+        }
+
+    }
+    )};
+
+exports.deleteUserProfile = (req,res,next) => {
+    db.query(`DELETE FROM users WHERE user_id=${req.body.user_id};`,(err,result,fields) =>{
+        if(err){
+            return res.status(400).json({err});
+        }
+        return res.status(200).json(result)
+        
+    }
+    )};
+    exports.modifyProfile = (req,res,next) => {
+        if (req.body.nom !=''){
+            db.query(`UPDATE users SET nom=? WHERE user_id=${req.body.user_id}`,[req.body.nom],(err,res) =>{
+                if (err) throw err;
+            });
+        }
+        if(req.body.prenom !=''){
+            db.query(`UPDATE users SET prenom=? WHERE user_id=${req.body.user_id}`,[req.body.prenom],(err,res) =>{
+                if (err) throw err;
+            });
+        }
+        if (req.body.email !=''){
+            db.query(`UPDATE users SET email=? WHERE user_id=${req.body.user_id}`,[req.body.email],(err,res) =>{
+                if (err) throw err;
+            });
+        }
+        return res.status(200).json({messsage:'le profil à été modifié'})
+        // if (req.body.password !=''){
+        //     bcrypt.hash(req.body.password, 10)
+        //     .then(hash =>{
+        //         db.query(`UDATE users SET password=? WHERE user_id=${req.body.user_id}`,[hash],
+        //         (err,res,field)=>{
+        //             if(err){
+        //                 return res.status(400).json({err});
+
+        //         }
+        //         return res.status(201).json({message:"Le mot de passe a été modifié"});
+            
+
+        //     })
+            
+        // });
+        
+        // }
+    }
