f133989d2b6e5135c6a8baeb4f3f66cfdfe97c9c
NicolasLegendre81 <86867910+NicolasLegendre81@users.noreply.github.com>
2022-02-09 15:06:52 +0100

    Add files via upload

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -10,7 +10,7 @@ router.post("/login",userCtrl.login);
 router.get("/user/profile/:id",auth,userCtrl.getUserProfile);
 router.delete("/user/profile/:id",auth,multer,userCtrl.deleteUserProfile);
 router.put ("/user/profile/:id",auth,multer,userCtrl.modifyProfile);
-router.put ("/user/profile/:id/password",auth,multer,userCtrl.modifyPassword);
-router.put ("/user/profile/:id/image",auth,multer,userCtrl.profilePic);
+router.put ("/user/profile/password/:id",auth,multer,userCtrl.modifyPassword);
+router.put ("/user/profile/image/:id",auth,multer,userCtrl.profilePic);
 
 module.exports = router;
