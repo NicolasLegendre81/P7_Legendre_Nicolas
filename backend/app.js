2e409e3280f85ec3c68081f22ade9528478804cc
NicolasLegendre81 <86867910+NicolasLegendre81@users.noreply.github.com>
2022-02-09 08:44:12 +0100

    Add files via upload

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const db = require ('./config/db')
 const bodyparser = require('body-parser');
 const path = require ('path');
 const userRoute = require ('./routes/user');
+const postRoute = require ('./routes/posts');
   
 const app = express();
 //middleware Cors
@@ -12,9 +13,9 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
-  app.use (bodyparser.json());
+app.use (bodyparser.json());
 
-app.use("/images", express.static(path.join(__dirname, "images")));
 app.use ('/api/auth',userRoute);
-//app.use ('/api/posts',postRoute);
+app.use ('/api/posts',postRoute);
+app.use("/images", express.static(path.join(__dirname, "images")));
 module.exports = app;
