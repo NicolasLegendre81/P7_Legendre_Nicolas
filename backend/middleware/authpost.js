5fc4e1ab2bdf05a4d6de298607e8482e1c68ab2a
NicolasLegendre81 <86867910+NicolasLegendre81@users.noreply.github.com>
2022-02-12 08:28:50 +0100

    Add files via upload

---
+++ backend/middleware/authpost.js
@@ -0,0 +1,14 @@
+const jwt = require ('jsonwebtoken');
+const db = require ('../config/db');
+module.exports = (req,res,next) =>{
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.verify(token,process.env.MY_TOKEN);
+    const user_id = decodedToken.user_id;
+    db.query(`SELECT user_id FROM users INNER JOIN posts ON author_id = users.user_id  WHERE id_post=${req.params.id}`,(err,result) =>{
+       if ((result[0].user_id === user_id)){ next();
+       }else{
+        res.status(401).json({ message:'Vous ne pouvez pas modifier cette publication'});
+    }
+    
+    });
+};
