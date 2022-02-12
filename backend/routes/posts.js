dad583e1cd71b8d08ffece4eb9b94de66a4e206e
NicolasLegendre81 <86867910+NicolasLegendre81@users.noreply.github.com>
2022-02-12 08:29:44 +0100

    Add files via upload

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,12 +1,13 @@
 const router = require ('express').Router();
 const auth = require("../middleware/auth");
+const authpost = require ("../middleware/authpost");
 const postCtrl = require ("../controllers/posts");
 const multer = require("../middleware/multer");
 
 router.get("/", auth,postCtrl.getAllPosts);
 router.post("/",auth,multer,postCtrl.createPost);
-router.put("/:id",auth,multer,postCtrl.modifyPost);
-// router.delete("/:id",auth,multer,postCtrl.deletePost);
+router.put("/:id",auth,authpost,multer,postCtrl.modifyPost);
+router.delete("/:id",auth,authpost,multer,postCtrl.deletePost);
 
 module.exports = router;
 
