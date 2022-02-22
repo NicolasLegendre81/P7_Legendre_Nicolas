8a0c117411daf3a817c3f47dcd9822c8985a0095
NicolasLegendre81 <86867910+NicolasLegendre81@users.noreply.github.com>
2022-02-22 15:47:16 +0100

    Add files via upload

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,164 +1,19 @@
-const db = require ('../config/db');
-const fs = require ('fs');
+const router = require ('express').Router();
+const auth = require("../middleware/auth");
+const authpost = require ("../middleware/authpost");
+const postCtrl = require ("../controllers/posts");
+const multer = require("../middleware/multer");
 
+router.get("/", auth,postCtrl.getAllPosts,);//accés a tout les posts
+router.post("/",auth,multer,postCtrl.createPost);//création de post depuis le fil d'actualité
+router.put("/:id",auth,authpost,multer,postCtrl.modifyPost);//modification de post depuis le fil d'actualité
+router.delete("/:id",auth,authpost,multer,postCtrl.deletePost);//suppression depuis le fil d'actualité
+router.get("/user/:id",auth,postCtrl.getPostsUser);//reception des posts d'un utilisateur
+router.post("/user/:id",auth,multer,postCtrl.createPost);//création d'un post depuis le profil
+router.post("/share/:id",auth,multer,postCtrl.sharePost);
+router.put("/user/:id",auth,authpost,multer,postCtrl.modifyPost);//modification d'un post depuis le profil
+router.delete("/user/:id",auth,authpost,multer,postCtrl.deletePost);//supression d'un post depuis le profil
 
-exports.getAllPosts = (req,res,next) =>{
-    
-        postPromise = () =>{
-            return new Promise((resolve,reject) =>{
-                db.query(`SELECT nom,prenom,position_in_company,imageUrl,id_post,post,post_date,author_id,post_imageUrl 
-                FROM users JOIN  posts WHERE author_id =user_id ORDER BY post_date DESC`,
-                (err,result)=>{
-                    if (err){return reject(error)}
-                    else{return resolve(result);}
-                    
-                });
-            });
-        }
-        sharePromise = () =>{
-            return new Promise((resolve,reject) =>{
-                db.query(`SELECT * FROM partage JOIN posts ON posts.id_post=partage.id_post 
-                JOIN users ON users.user_id=partage.user_id ORDER BY share_date DESC`,(err,result)=>{
-                    if (err){return reject(error)}
-                    else{return resolve (result);}
-                });
+module.exports = router;
 
-            });
-        };
-        async function getShareNallPost () {
-            const promise1= postPromise();
-            const promise2= sharePromise();
-            const promises =[promise1,promise2];
-             
-            try{
-            const result = await Promise.all(promises);
-             
-            return res.status(201).json(result)
-             
-            } catch(error){
-            console.log(error)
-            }
-            }
-            getShareNallPost()
-
-};
-exports.getPostsUser = (req,res,next) => {
-    userPostPromise = () =>{
-        return new Promise((resolve,reject) =>{
-            db.query(`SELECT * FROM posts JOIN
-             (SELECT user_id,nom,prenom,position_in_company,imageUrl FROM users)
-             AS userposts ON user_id=posts.author_id WHERE user_id =${req.params.id} 
-             ORDER BY post_date DESC`,(error,result)=>{
-                if (error){return reject(error)}
-                else{return resolve(result);}
-
-             });
-
-        });
-    }
-    userSharePromise = () =>{
-        return new Promise((resolve,reject) =>{
-            db.query(`SELECT * FROM partage JOIN posts ON posts.id_post=partage.id_post 
-            JOIN users ON users.user_id=partage.user_id WHERE users.user_id =${req.params.id} 
-            ORDER BY share_date DESC`,(error,result)=>{
-                if(error){return reject(error)}
-                else{return resolve (result);}
-            });
-        });
-    }
-    async function getUserShareNUserPost () {
-        const promise1= userPostPromise();
-        const promise2= userSharePromise();
-        const promises =[promise1,promise2];
-        try{
-            const result = await Promise.all(promises);
-             
-            return res.status(201).json(result)
-        }catch(error){console.error(error)};
-    
-};
-getUserShareNUserPost ()
-}
-exports.createPost = (req,res,next) => {
-    try{
-    const image = (req.file) ? `${req.protocol}://${req.get('host')}/images/${req.file.filename}` : "";
-    db.query (`INSERT INTO posts (post,author_id,post_imageUrl) 
-    VALUES (?,?,?)`,[req.body.post,req.body.user_id,image],(err,result) =>{
-        if (err){
-            throw err
-        }else{
-            res.status(201).json({message :'Votre poste a été publié'});
-        }    
-    });
- }catch(error){console.error[err]}
-};
-
-exports.modifyPost = (req,res,next) =>{
-    
-    if (req.file){
-        db.query(`SELECT * FROM posts WHERE id_post = ${req.params.id}`, (err, result) =>{
-            if(err) res.status(400).json ({err});
-            if (result[0].post_imageUrl !=""){
-                const filename = result[0].post_imageUrl.split('/images/')[1];
-                fs.unlink (`images/${filename}`,()=>{
-                    if(err) {throw err;}
-                    const image = (req.file) ? `${req.protocol}://${req.get('host')}/images/${req.file.filename}` : "";
-                    db.query(`UPDATE posts SET post="${req.body.post}", post_imageUrl="${image}" 
-                    WHERE id_post = ${req.params.id}`,(err,result) =>{
-                        if(err){throw err;}
-                        return res.status(201).json({message:'Publication modifiée'})
-                    })
-                });
-            }else{
-                const image = (req.file) ? `${req.protocol}://${req.get('host')}/images/${req.file.filename}` : "";
-                    db.query(`UPDATE posts SET post="${req.body.post}", post_imageUrl="${image}" 
-                    WHERE id_post = ${req.params.id}`,(err,result) =>{
-                        if(err){throw err;}
-                        return res.status(201).json({message:'Publication modifiée'})
-                    });   
-            }
-        });
-    }else{
-        db.query(`UPDATE posts SET post="${req.body.post}" 
-        WHERE id_post=${req.params.id}`,(err,result) =>{
-            if(err){throw err;}
-            return res.status(201).json({message:'Publication modifiée'})
-        });
-    }
-};
  
-
-exports.deletePost = (req,res,next) => {
-    try{
-    db.query( `SELECT * FROM posts WHERE id_post = ${req.params.id}`, (err,result) =>{
-        if (err){throw err;}
-       if(result[0].imageUrl !=""){
-        const filename = result[0].post_imageUrl.split('/images/')[1];
-        fs.unlink(`images/${filename}`,() =>{
-            db.query(`DELETE FROM posts WHERE id_post = ${req.params.id}`,(err,result) =>{
-                if (err){throw err;}
-                return res.status(201).json({message:'La publication a été suppprimée'})
-            });
-        })
-       }else{
-        db.query(`DELETE FROM posts WHERE id_post = ${req.params.id}`,(err,result) =>{
-            if (err){throw err;}
-            return res.status(201).json({message:'La publication a été suppprimée'})
-        });
-       }
-    });
-    }catch(error){console.error(err)}
-};
-exports.sharePost = (req,res,next) => {
-    try{
-        db.query(`INSERT INTO partage (id_post,user_id)
-         VALUES (?,?)`,[req.params.id,req.body.id],(err,result) =>{
-            if (err){throw err;}
-            return res.status(201).json({message:'Nouveau partage publié'})
-        }
-        );
-
-    }catch(error){console.error(err)}
-
-    };
-
