808f590b5fd27d2b45e99e88c0ede00d59154620
NicolasLegendre81 <86867910+NicolasLegendre81@users.noreply.github.com>
2022-02-24 14:30:01 +0100

    Delete src directory

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import axios  from 'axios'
-import router from './router'
-Vue.prototype.$http = axios
-
-createApp(App).use(router).mount('#app')
