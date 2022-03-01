1663732806cbfd3475cad93d10be0df663d09d02
NicolasLegendre81 <86867910+NicolasLegendre81@users.noreply.github.com>
2022-03-01 08:18:47 +0100

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,26 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-
+/* eslint-disable */
+import HomePage from "../views/HomePage.vue"
+import SignUp from "../components/SignUp.vue"
+import LoGin from "../components/LoGin.vue"
 const routes = [
   {
-    // path: '/',
-    // name: 'home',
-    // component: HomeView
+    path: '/',
+    name: 'HomePage',
+    component: HomePage,
   },
   {
-    // path: '/about',
-    // name: 'about',
-    // // route level code-splitting
-    // // this generates a separate chunk (about.[hash].js) for this route
-    // // which is lazy-loaded when the route is visited.
-    // component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path:'/signup',
+    name:'SignUp',
+    component:SignUp,
+  },
+  {
+    path:'/login',
+    name:'LoGin',
+    components:LoGin,
+  },
+
+
 ]
 
 const router = createRouter({
