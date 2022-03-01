61effad9a505e16c8a39e02dd845ad867b4d6ba4
NicolasLegendre81 <86867910+NicolasLegendre81@users.noreply.github.com>
2022-03-01 15:32:07 +0100

    Add files via upload

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,13 +3,9 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, process.env.MY_TOKEN);
-    const user_id = decodedToken.user_id;
-    if (req.body.user_id && req.body.user_id !== user_id) {
-      throw 'userId invalide!';
-    } else {
-      next();
-    }
+   jwt.verify(token, process.env.MY_TOKEN);
+    next()
+    
   } catch {
     res.status(401).json({
       error: new Error('La requête est invalide!')
