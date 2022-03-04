50faaa6f8d15977596fa5415da1aa61422d63fd7
NicolasLegendre81 <86867910+NicolasLegendre81@users.noreply.github.com>
2022-03-04 08:23:25 +0100

    Add files via upload

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="home-container">
-        <SignUp/>
+      
+        <Signup/>
+        <Login/>
         <div class="welcome-message">
             <h3>
                 Bienvenue sur le réseau social de VOTRE entreprise!
@@ -17,7 +19,7 @@
     font-size: 2em;
     color: rgb(99, 99, 100);
     grid-column: 2/5;
-    grid-row: 4/5;
+    grid-row: 1/2;
     text-shadow: rgb(209, 208, 205) 1px 0 2px;
     text-align: center;
     position:relative;
@@ -28,15 +30,30 @@
 .home-container{
     display:grid;
     grid-template-columns: 1fr 2fr 1fr 2fr 1fr;
-    grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr 1fr ;
+    box-sizing: border-box;
+    background:black no-repeat;
+    background-image: url('~@/assets/Background.png');
+}
+
+img{
+    margin:0;
+    padding:0;
 }
 </style>
 <script>
-import SignUp  from '@/components/SignUp.vue'
+import Signup  from '../components/Signup.vue';
+import Login from'../components/Login.vue';
 
 export default ({
     name:"HomePage",
 
-   components:{ SignUp}
-})
+   components:{ 
+       Signup,
+       Login,
+   },
+   
+});
+
+
 </script>
