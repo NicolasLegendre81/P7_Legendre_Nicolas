b19d0622bee1f192a8de26285b1ac066450e0722
NicolasLegendre81 <86867910+NicolasLegendre81@users.noreply.github.com>
2022-03-08 08:02:49 +0100

    Add files via upload

---
+++ src/views/WallPage.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="WallPage_container">
+        <GetallPosts/>
+
+    </div>
+
+</template>
+<script>
+import GetallPosts from '../components/GetAllPosts.vue';
+export default ({
+    name:"WallPage",
+
+   components:{ 
+       GetallPosts,
+      
+   },
+   
+});
+
+
+
+</script>
+<style scoped>
+.WallPage_container{
+display: grid;
+grid-template-columns: 1fr 2fr 1fr 2fr 1fr;
+grid-template-rows: 1fr 5fr 1fr;
+box-sizing: border-box;
+background:#1F1B1B ;
+}
+</style>
