d4a1adcccf1e52547e67f9d8e2004d16d2d6d370
NicolasLegendre81 <86867910+NicolasLegendre81@users.noreply.github.com>
2022-05-03 10:42:30 +0200

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 /* eslint-disable */
 import HomePage from "../views/HomePage.vue"
 import WallPage from "../views/WallPage.vue"
+import Signup from "../components/Signup.vue"
+import Login from "../components/Login.vue"
 
 const routes = [
   {
@@ -13,7 +15,20 @@ const routes = [
     path:'/post',
     name:'WallPage',
     component:WallPage,
+  },
+  {
+    path:'/signup',
+    name: 'Signup',
+    component: Signup,
+
+  },
+  {
+    path:'/login',
+    name: 'Login',
+    component: Login,
   }
+
+
 
 ]
 
