ad881a75711eb6b6477cf315a98c6477d7b1982e
NicolasLegendre81 <86867910+NicolasLegendre81@users.noreply.github.com>
2022-05-03 10:43:14 +0200

    Add files via upload

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,7 +1,6 @@
 <template>
+<div class="container-fluid">
     <div class="home-container">
-      
-        <Signup/>
         <Login/>
         <div class="welcome-message">
             <h3>
@@ -11,45 +10,36 @@
         </div>
 
     </div>
-        
+</div>      
     
 </template>
 <style scoped>
 .welcome-message{
     font-size: 2em;
     color: rgb(99, 99, 100);
-    grid-column: 2/5;
-    grid-row: 1/2;
     text-shadow: rgb(209, 208, 205) 1px 0 2px;
     text-align: center;
-    position:relative;
+    margin-top: 3em;
+    position:sticky;
 
     
     
 }
-.home-container{
-    display:grid;
-    grid-template-columns: 1fr 2fr 1fr 2fr 1fr;
-    grid-template-rows: 1fr 1fr 1fr 1fr ;
-    box-sizing: border-box;
-    background:black no-repeat;
-    background-image: url('~@/assets/Background.png');
-}
+
 
 img{
     margin:0;
     padding:0;
 }
+
 </style>
 <script>
-import Signup  from '../components/Signup.vue';
 import Login from'../components/Login.vue';
 
 export default ({
     name:"HomePage",
 
    components:{ 
-       Signup,
        Login,
    },
    
--- src/views/WallPage.vue
+++ src/views/WallPage.vue
@@ -1,9 +1,10 @@
 <template>
-    <div class="WallPage_container">
-        <GetallPosts/>
+    <div class=container-fluid>
+        <div class="WallPage_container">
+            <GetallPosts/>
 
+        </div>
     </div>
-
 </template>
 <script>
 import GetallPosts from '../components/GetAllPosts.vue';
@@ -21,11 +22,5 @@ export default ({
 
 </script>
 <style scoped>
-.WallPage_container{
-display: grid;
-grid-template-columns: 1fr 2fr 1fr 2fr 1fr;
-grid-template-rows: 1fr 5fr 1fr;
-box-sizing: border-box;
-background:#1F1B1B ;
-}
+
 </style>
