05078fe018b7cb15bfa82a0e8dbcff511633fc4a
NicolasLegendre81 <86867910+NicolasLegendre81@users.noreply.github.com>
2022-05-18 11:44:26 +0200

    Add files via upload

--- backend/controllers/comments.js
+++ backend/controllers/comments.js
@@ -1,8 +1,30 @@
-const router = require ('express').Router();
-const auth = require("../middleware/auth");
-const commentsCtrl =require("../controllers/comments");
+const fs = require ('fs');
+const db = require ('../config/db');
 
-// router.get("/", auth,commentsCtrl.getComs,);//accés a tout les commentaires
-router.post("/",auth,commentsCtrl.createCom);
 
-module.exports = router;
+exports.createCom=(req,res,next) =>{
+    try{
+        db.query(`INSERT INTO comments (comment_content,post_id,comment_author_id) VALUES (?,?,?)`,
+        [req.body.comment_content,req.body.post_id,req.body.user_id],(err,result) =>{
+        if (err){
+            throw err
+        }else{
+            res.status(201).json({message :'Votre commentaire a été publié'});
+        }
+        });    
+    }catch(error){console.error[err]}
+
+};
+// exports.getComs = (req,res,next) =>{
+    
+//     getComsPromise = () =>{
+//         return new Promise((resolve,reject) =>{
+//             db.query(`SELECT * FROM comments JOIN posts ON posts.id_post=comments.post-id`,
+//             (err,result)=>{
+//                 if (err){return reject(error)}
+//                 else{return resolve(result);}
+                
+//             });
+//         });
+//     }
+// }
