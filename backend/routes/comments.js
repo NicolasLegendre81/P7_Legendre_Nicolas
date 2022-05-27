45be159c66ee45021db4a779bad58c0b8d813802
NicolasLegendre81 <86867910+NicolasLegendre81@users.noreply.github.com>
2022-05-27 12:35:01 +0200

    Add files via upload

--- backend/routes/comments.js
+++ backend/routes/comments.js
@@ -1,8 +1,11 @@
 const router = require ('express').Router();
 const auth = require("../middleware/auth");
+const authcoms = require('../middleware/authcoms')
 const commentsCtrl =require("../controllers/comments");
 
-// router.get("/", auth,commentsCtrl.getComs,);//accés a tout les commentaires
+router.get("/:id",auth,commentsCtrl.getComs);//accés a tout les commentaires
 router.post("/",auth,commentsCtrl.createCom);
+router.delete("/:id",auth,authcoms,commentsCtrl.deleteComs);
+router.put("/:id",auth,authcoms,commentsCtrl.modifyComs);
 
 module.exports = router;
